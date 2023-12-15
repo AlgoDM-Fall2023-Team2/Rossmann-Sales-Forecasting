fce2a4dab25d0583ff775e8428f765a96205ed36
Vasundhara Sharma <65170222+sharma-vasundhara@users.noreply.github.com>
2023-12-15 08:26:55 -0500

    project layout

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# page headings
+st.set_page_config(layout="wide", page_title="INFO7374: Algorithmic Digital Marketing")
+st.title("Rossmann Sales Prediction")
+st.header("INFO7374: Final Project")
+st.subheader("Team 2: Adit Bhosale, Sowmya Chatti, Vasundhara Sharma")
+
